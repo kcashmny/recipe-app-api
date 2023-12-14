0208b87048f2c3e9425262aafd1913e1337f61c7
kcashmny <kevincashwell@gmail.com>
2023-12-14 08:35:14 -0500

    new test 2

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,13 +10,15 @@ class CalcTests(SimpleTestCase):
 
     def test_add_number(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subract_numbers(self):
         """Test subtracting numbers."""
 
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
+
+
